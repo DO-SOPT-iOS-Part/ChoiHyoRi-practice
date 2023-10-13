3fb4501208e597fb281d7fa1ab1667d04633b5bb
Hyori-Choi <112244094+hyoring030@users.noreply.github.com>
2023-10-13 23:25:33 +0900

    Revert "솝트 1주차 과제-로그인 페이지 구현"

--- sopt33-first-seminar/sopt33-first-seminar/ViewController.swift
+++ sopt33-first-seminar/sopt33-first-seminar/ViewController.swift
@@ -16,13 +16,21 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        // Do any additional setup after loading the view.
     }
+    
+//    @IBAction func idTextFieldDidEditing(_ sender: Any) {
+//        guard let textField = sender as? UITextField else {return}
+//        if let idText = textField.text{
+//            self.idText=idText
+//        }
+//    }
     
     @IBAction func idTextFieldDidEditing(_ sender: Any) {
         guard let textField = sender as? UITextField else {return}
-        if let idText = textField.text{
-            self.idText=idText
-        }
+        //        if let idText = textField.text{
+        //            self.idText=idText
+        //        }
     }
     @IBAction func passwordTextFieldDidEditing(_ sender: Any) {
         guard let textField = sender as? UITextField else {return}
@@ -31,47 +39,28 @@ class ViewController: UIViewController {
         }
     }
     
-    
-    
-    
     @IBAction func loginButtonTap(_ sender: Any) {
         print("\(idText)\n\(passwordText)")
-//        pushToResultVC() //네비게이션
-        presentToResultVC() //모달 방식
-        
+        pushToResult()
     }
-    // \()로 문자열을 출력할 수 있음 : 문자열 보간법
     
-    
-    func pushToResultVC(){
+    func pushToResult(){
         guard let resultVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
-//        resultVC.email = idText
-//        resultVC.passwordText = passwordText
+        resultVC.email = idText
+        resultVC.password = passwordText
         resultVC.delegate=self
-        resultVC.setLabelText(id: idText, password: passwordText)
-        self.navigationController?.pushViewController(resultVC, animated: true)
-//        self.present(resultVC, animated: true)
-        resultVC.loginDataCompletion = { data in
-            print("클로저로 받아온 email : \(data[0]), 클로저로 받아온 password : \(data[1])")
-        }
-        guard let resultVC = self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else {return}
-                self.navigationController?.pushViewController(resultVC, animated: true)
-    }
-    
-    func presentToResultVC(){ //아래에서 위로 올라오는 모달
-        //프로퍼티 사용
-        guard let resultVC =   self.storyboard?.instantiateViewController(withIdentifier: "ResultViewController") as? ResultViewController else{return}
-//        resultVC.email=idText
-//        resultVC.passwordText=passwordText
-        resultVC.setLabelText(id: idText, password: passwordText) //함수를 이용한 데이터 전달
+        //        self.navigationController?.pushViewController(resultVC, animated: true)
         self.present(resultVC, animated: true)
+        resultVC.loginDataCompletion = { data in
+                  print("클로저로 받아온 email : \(data[0]), 클로저로 받아온 password : \(data[1])")
+              }
     }
 }
-    //함수 구현부
-    extension ViewController: GetDataProtocol {
-        func getLoginData(email: String, password: String) {
-            print("받아온 email : \(email), 받아온 password : \(password)")
-        }
-        
-        
+
+extension ViewController: GetDataProtocol {
+    func getLoginData(email: String, password: String) {
+        print("받아온 email : \(email), 받아온 password : \(password)")
     }
+    
+    
+}
